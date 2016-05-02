24f7853465d3511adf3d89190a52fcdeb15a2c00
Nathaniel Blackburn <support@nblackburn.uk>
2016-05-03 00:20:16 +0100

    Improved comments

--- app/initialize.js
+++ app/initialize.js
@@ -3,22 +3,46 @@ import 'babel-polyfill';
 import store from './state/store';
 import VueRouter from 'vue-router';
 import routes from './config/routes';
-import VueResource from 'vue-resouce';
 import VueValidator from 'vue-validator';
+
+//
+// Vue extensions
+// Here we tell Vue that we want to add extra functionality to the application.
+//
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(VueValidator);
 
+// 
+// Application
+// Here we will create the application instance.
+// 
+
 let application = {
     store
 };
+
+// 
+// Router
+// Here we will create the router instance.
+// 
 
 let router = new VueRouter({
     history: true
 });
 
+// 
+// Router mapping.
+// Here we will tell VueRouter what routes the application will be using.
+// 
+
 router.map(routes);
+
+// 
+// Start the application
+// Finally, we can start the application.
+// 
 
 router.start(application, '#content', function() {
     
