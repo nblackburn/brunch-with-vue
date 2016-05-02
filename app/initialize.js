1bfcda80421dfed53a49212114061742ab107ee3
Nathaniel Blackburn <support@nblackburn.uk>
2016-05-03 00:19:49 +0100

    Added vue-resource and vue-validator

--- app/initialize.js
+++ app/initialize.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import 'babel-polyfill';
-import VueRouter from 'vue-router';
-
 import store from './state/store';
+import VueRouter from 'vue-router';
 import routes from './config/routes';
+import VueResource from 'vue-resouce';
+import VueValidator from 'vue-validator';
 
 Vue.use(VueRouter);
-Vue.config.debug = true;
+Vue.use(VueResource);
+Vue.use(VueValidator);
 
 let application = {
     store
