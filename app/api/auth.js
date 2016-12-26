a5ab044aa37613338c3b6f2ce338f9622443db97
lubien <lubien1996@gmail.com>
2016-12-26 16:05:58 -0300

    Update State.dispatch to State.commit
    It's the new Vuex method to dispatch mutations

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
 import State from '../state/store';
 import VueResource from 'vue-resource';
+import {
+    AUTH_TOKEN,
+} from '../state/mutations';
 
 Vue.use(VueResource);
 
 export default {
 
     signin: function(username, password) {
-        State.dispatch('AUTH_TOKEN', 'Q6SnSk2N5wGbXsEllqG6hSx7vRmxcz');
+        State.commit(AUTH_TOKEN, 'Q6SnSk2N5wGbXsEllqG6hSx7vRmxcz');
     },
 
     signout: function() {
-        State.dispatch('AUTH_TOKEN', null);
+        State.commit(AUTH_TOKEN, null);
     }
 }
